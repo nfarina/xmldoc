f9f38d1d96497401232bfa60efc83390aae435c3
Mirko Buholzer <mirko@buholzer.com>
2016-04-26 18:39:17 -0700

    Add test to validate escaping of &lt; &gt; [#29]

--- test/issues.js
+++ test/issues.js
@@ -7,3 +7,12 @@ t.test('parsing comments outside XML scope [#27]', function (t) {
   var parsed = new XmlDocument(xmlString);
   t.end();
 })
+
+t.test('validating escaping of &lt; &gt; [#29]', function (t) {
+  
+  var xmlString = '<root><command>&lt; &gt;</command></root>';
+  var parsed = new XmlDocument(xmlString);
+  var result = parsed.toString({compressed:true})
+  t.equal(result, xmlString);
+  t.end();
+})
